04d98a0f8abc2ddceb41ec0df547d0f56e690800
colla0220 <gabri@DESKTOP-G05AM4L>
2026-09-19 16:57:19 -0300

    bah

--- src/main/java/net/mcreator/minerp/client/renderer/block/VerCelularCarregandoNaBaseBlockEntityRenderer.java
+++ src/main/java/net/mcreator/minerp/client/renderer/block/VerCelularCarregandoNaBaseBlockEntityRenderer.java
@@ -61,10 +61,11 @@ public class VerCelularCarregandoNaBaseBlockEntityRenderer implements BlockEntit
 				stack = container.getItem(0);
 			if (!stack.isEmpty()) {
 				poseStack.pushPose();
-				poseStack.translate(0.49, 0.22999999999999998, 0.55);
-				poseStack.mulPose(Axis.XP.rotationDegrees(155.5f));
-				poseStack.mulPose(Axis.YP.rotationDegrees(360f));
-				poseStack.mulPose(Axis.ZP.rotationDegrees(180f));
+				poseStack.translate(0.5, 0.5, 0.5);
+				applyFacingRotation(poseStack, state);
+				poseStack.translate(-0.01, -0.27000000000000002, 0.05);
+				poseStack.mulPose(Axis.XP.rotationDegrees(20f));
+				poseStack.mulPose(Axis.YP.rotationDegrees(180f));
 				float _scale0 = 0.55f;
 				poseStack.scale(_scale0, _scale0, _scale0);
 				itemRenderer.renderStatic(stack, ItemDisplayContext.FIXED, packedLight, packedOverlay, poseStack, bufferSource, level, 0);
